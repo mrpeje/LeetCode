e93af464571a3bfe35932ede1eb1542bcdaae791
MRPEJE <whond.earn.war@gmail.com>
2024-09-21 14:44:50 +0400

    141. Linked List Cycle 99 ms Beats 30.76% 46.27 MB Beats 98.08%

---
+++ 141.LinkedListCycle/Program.cs
@@ -0,0 +1,30 @@
+﻿// O(n)
+public class ListNode 
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+        while (fastPtr != null)
+        {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next;
+            if (slowPtr == null || fastPtr == null)
+                return false;
+            fastPtr = fastPtr.next;
+            if (slowPtr == fastPtr)
+                return true;
+        }
+        return false;
+    }
+}
