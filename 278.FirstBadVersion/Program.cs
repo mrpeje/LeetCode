95d7f9a9100dc59d72773496fed586335ec9998d
MRPEJE <whond.earn.war@gmail.com>
2024-09-23 19:32:37 +0400

    278. First Bad Version 28 ms Beats 15.54% 26.81 MB Beats 40.15%

---
+++ 278.FirstBadVersion/Program.cs
@@ -0,0 +1,27 @@
+﻿// O(logN)
+public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        int low = 1;
+        int high = n;
+        int mid = 0;
+        while (low <= high)
+        {
+            mid = low + (high - low) / 2;
+            if (IsBadVersion(mid))
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+
+        }
+        if (IsBadVersion(mid))
+            return mid;
+        else
+            return mid + 1;
+    }
+}
