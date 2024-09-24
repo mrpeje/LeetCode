1ac6df773ef9b900a5fd5039c38b36c0b14af3e1
MRPEJE <whond.earn.war@gmail.com>
2024-09-24 15:20:53 +0400

    409. Longest Palindrome 49 ms Beats 61.26% 40.34 MB Beats 5.18%

---
+++ 409.LongestPalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+public class Solution
+{
+    public int LongestPalindrome(string s)
+    {
+        Hashtable ht = new Hashtable();
+        int palindromeSize = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (ht.Contains(s[i]))
+            {
+                ht.Remove(s[i]);
+                palindromeSize += 2;
+            }
+            else
+            {
+                ht.Add(s[i], i);
+            }
+        }
+        if(ht.Count > 0)
+        {
+            palindromeSize++;
+        }
+        return palindromeSize;
+    }
+}
