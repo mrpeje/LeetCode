6052e9cfd8204a93e425e18ecae461c2adfb475b
MRPEJE <whond.earn.war@gmail.com>
2024-09-26 13:24:43 +0400

    169. Majority Element 113 ms Beats 65.18% 48.84 MB Beats 34.71%

---
+++ 169.MajorityElement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+var nums = new int[] { 6, 5, 5 };
+
+var sol = new Solution();
+var res = sol.MajorityElement(nums);
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        var ht = new Dictionary<int, int>();
+        var major = (nums[0], 1);
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (ht.ContainsKey(nums[i]))
+            {
+                ht[nums[i]]++;
+                if (major.Item2 < ht[nums[i]])
+                {
+                    major.Item1 = nums[i];
+                    major.Item2 = ht[nums[i]];
+
+                }
+            }
+            else
+                ht[nums[i]] = 1;
+        }
+        return major.Item1;
+    }
+}
