576a8dd0229cfe43f67b94898424876491c9c261
MRPEJE <whond.earn.war@gmail.com>
2024-09-29 13:22:40 +0400

    3. Longest Substring Without Repeating Characters 184 ms Beats 6.38% 120.76 MB Beats 13.08%

---
+++ 3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+// O(n)
+using System.Collections;
+var solution = new Solution();
+var res = solution.LengthOfLongestSubstring("dvdf");
+Console.WriteLine(res);
+Console.ReadLine();
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if(s.Length == 0)
+            return 0;
+        Hashtable hashtable = new Hashtable();
+        int currentMax = 0;
+        int prevMax = 0;
+
+        for (int j = 0; j < s.Length; j++)
+        {
+            if (prevMax > s.Length - j)
+                break;
+            for (int i = j; i < s.Length; i++)
+            {
+                if (hashtable.ContainsKey(s[i]))
+                {
+
+                    break;
+                }
+                else
+                {
+                    hashtable.Add(s[i], i);
+                    currentMax++;
+                }
+            }
+            if (prevMax < currentMax)
+                prevMax = currentMax;
+            currentMax = 0;
+            hashtable.Clear();
+        }
+        if (prevMax < currentMax)
+            return currentMax;
+        else
+            return prevMax;
+    }
+}
